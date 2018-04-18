374a6abb854cafdf65f97e6bb22788b60e722d1b
Jonathan Bobel <jonathan.bobel@sherwin.com>
2018-04-18 16:04:54 -0400

    IE11 Fix for object.keys

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,20 @@ $(function() {
 	  }
 	});
 
+	// Stupid fix for IE11 
+	Object.keys = function(obj) {
+	    var keys = [];
+
+	    for (var i in obj) {
+	      if (obj.hasOwnProperty(i)) {
+	        keys.push(i);
+	      }
+	    }
+
+	    return keys;
+	  };
+
+
 	//Tooltip functionality 
 	$('[data-toggle="tooltip"]').tooltip();
 
