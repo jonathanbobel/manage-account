7d2cf6efaf865aac4519e5c334e9415619141654
Jonathan Bobel <jonathan.bobel@sherwin.com>
2018-04-18 11:32:05 -0400

    Reset Password screen push

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,35 @@
 $(function() {
     
+	//Extend accordion functionality 
+	$('label.control.control--radio.panel-title > input[data-toggle="collapse"]').click(function(e){
+	  target = $(this).attr('data-target')
+	  console.log(target);
+	  if ($(target).hasClass('show')) {
+	    e.preventDefault(); // to stop the page jump to the anchor target.
+	    e.stopPropagation()
+	  }
+	});
+
+	//Tooltip functionality 
+	$('[data-toggle="tooltip"]').tooltip();
+
+	 // Set current year as max for birthday 
+	var today = new Date();
+	var yyyy = today.getFullYear();
+	document.getElementById("dateOfBirthYear").setAttribute("max", yyyy);
+
+	// Restrict input values 
+	 $( "input" ).change(function() {
+	   var max = parseInt($(this).attr('max'));
+	   var min = parseInt($(this).attr('min'));
+	   if ($(this).val() > max)
+	   {
+	      $(this).val(max);
+	   }
+	   else if ($(this).val() < min)
+	   {
+	      $(this).val(min);
+	   }       
+	 }); 
+
 });
